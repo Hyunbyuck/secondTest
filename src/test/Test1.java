e0db65255a54c3fde30bdaff9e1a8c804775bf97
Shings0310 <Shings0310@gmail.com>
2023-09-21 12:40:24 +0900

    받아라

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,5 +3,9 @@ package test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(1);
+        System.out.println("명령을");
+        System.out.println("하달한다");
+        System.out.println("얼간이");
     }
 }
